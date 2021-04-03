ccc3bbf59e32687460da1c01be82f9c39f96be82
tungtt0806 <57852063+tungtt0806@users.noreply.github.com>
2021-04-03 12:08:05 -0400

    Add files via upload

---
+++ FinalProject/FinalProject/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace FinalProject
+{
+    partial class makeDealForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.door1 = new System.Windows.Forms.Button();
+            this.door2 = new System.Windows.Forms.Button();
+            this.door3 = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.choiceButton = new System.Windows.Forms.Button();
+            this.rewardLabel = new System.Windows.Forms.Label();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // door1
+            // 
+            this.door1.BackColor = System.Drawing.Color.Peru;
+            this.door1.BackgroundImage = global::FinalProject.Properties.Resources.door;
+            this.door1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.door1.ForeColor = System.Drawing.Color.Black;
+            this.door1.Location = new System.Drawing.Point(44, 114);
+            this.door1.Name = "door1";
+            this.door1.Size = new System.Drawing.Size(145, 208);
+            this.door1.TabIndex = 0;
+            this.door1.Text = "Door 1";
+            this.door1.UseVisualStyleBackColor = false;
+            this.door1.Click += new System.EventHandler(this.Door1_Click);
+            // 
+            // door2
+            // 
+            this.door2.BackColor = System.Drawing.Color.Peru;
+            this.door2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.door2.ForeColor = System.Drawing.Color.Black;
+            this.door2.Image = global::FinalProject.Properties.Resources.door;
+            this.door2.Location = new System.Drawing.Point(276, 114);
+            this.door2.Name = "door2";
+            this.door2.Size = new System.Drawing.Size(145, 208);
+            this.door2.TabIndex = 1;
+            this.door2.Text = "Door 2";
+            this.door2.UseVisualStyleBackColor = false;
+            this.door2.Click += new System.EventHandler(this.Door2_Click);
+            // 
+            // door3
+            // 
+            this.door3.BackColor = System.Drawing.Color.Peru;
+            this.door3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.door3.ForeColor = System.Drawing.Color.Black;
+            this.door3.Image = global::FinalProject.Properties.Resources.door;
+            this.door3.Location = new System.Drawing.Point(505, 114);
+            this.door3.Name = "door3";
+            this.door3.Size = new System.Drawing.Size(145, 208);
+            this.door3.TabIndex = 2;
+            this.door3.Text = "Door 3";
+            this.door3.UseVisualStyleBackColor = false;
+            this.door3.Click += new System.EventHandler(this.Door3_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.Black;
+            this.title.Location = new System.Drawing.Point(183, 42);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(331, 31);
+            this.title.TabIndex = 3;
+            this.title.Text = "Let\'s Make a Deal Game";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label1.Location = new System.Drawing.Point(202, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Click on a Door to get your reward";
+            // 
+            // choiceButton
+            // 
+            this.choiceButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.choiceButton.ForeColor = System.Drawing.Color.Black;
+            this.choiceButton.Location = new System.Drawing.Point(265, 358);
+            this.choiceButton.Name = "choiceButton";
+            this.choiceButton.Size = new System.Drawing.Size(167, 38);
+            this.choiceButton.TabIndex = 6;
+            this.choiceButton.Text = "Trade Prize";
+            this.choiceButton.UseVisualStyleBackColor = true;
+            this.choiceButton.Click += new System.EventHandler(this.ChoiceButton_Click);
+            // 
+            // rewardLabel
+            // 
+            this.rewardLabel.AutoSize = true;
+            this.rewardLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rewardLabel.Location = new System.Drawing.Point(105, 412);
+            this.rewardLabel.Name = "rewardLabel";
+            this.rewardLabel.Size = new System.Drawing.Size(0, 24);
+            this.rewardLabel.TabIndex = 7;
+            this.rewardLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // restartButton
+            // 
+            this.restartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restartButton.ForeColor = System.Drawing.Color.Black;
+            this.restartButton.Location = new System.Drawing.Point(265, 471);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(167, 38);
+            this.restartButton.TabIndex = 8;
+            this.restartButton.Text = "Restart game";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // makeDealForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FinalProject.Properties.Resources.sea;
+            this.ClientSize = new System.Drawing.Size(707, 579);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.rewardLabel);
+            this.Controls.Add(this.choiceButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.door3);
+            this.Controls.Add(this.door2);
+            this.Controls.Add(this.door1);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Name = "makeDealForm";
+            this.Text = "That Tung Ton";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button door1;
+        private System.Windows.Forms.Button door2;
+        private System.Windows.Forms.Button door3;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button choiceButton;
+        private System.Windows.Forms.Label rewardLabel;
+        private System.Windows.Forms.Button restartButton;
+    }
+}
+
---
+++ FinalProject/FinalProject/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class makeDealForm : Form
+    {
+        public string[] trash_prizes = {"a candy", "a penny", "a trash bag", "an old shirt", "a coke bottle", "a cotton swabs", "a dried flower", "a broken mouse", "air", "fake jean"};
+        public string[] medium_prizes = {"a TV Sony", "$200", "a perfume bottle", "a speaker", "a pair of Nike sneaker", "a monitor", "a CPU", "a GPU", "a washing machine", "a fryer"};
+        public string[] amazing_prizes = {"a Lamborghini car", "a house", "$1 million", "a yatch", "a RV", "a 10 Carat diamond ring", "1000 Amazon shares", "a plane", "an island", "$100 million"};
+        public int[] door = { 1, 2, 3 };
+        public int randomDoor;
+        public int randomPrize1, randomPrize2, randomPrize3;
+        public bool choice = true;
+        public static Random r = new Random();
+        public static Random d = new Random();
+        public bool door1WasClicked = false;
+        public bool door2WasClicked = false;
+        public bool door3WasClicked = false;
+
+        public makeDealForm()
+        {
+            InitializeComponent();
+            randomDoor = d.Next(0, 3);
+            randomPrize1 = r.Next(0, trash_prizes.Length);
+            randomPrize2 = r.Next(0, medium_prizes.Length);
+            randomPrize3 = r.Next(0, amazing_prizes.Length);
+            choiceButton.Enabled = false;
+        }
+
+
+        public void Door1_Click(object sender, EventArgs e)
+        {
+            choiceButton.Enabled = true;
+            door1WasClicked = true;
+            if (choice)
+            {
+                door1.Tag = randomDoor;
+                if(randomDoor == 0)
+                {
+                    door2.Tag = 1;
+                    door3.Tag = 2;
+                    door1.Text = trash_prizes[randomPrize1];
+                    rewardLabel.Text = "You won " + trash_prizes[randomPrize1] + "." + "You can change doors now or get the prize!";
+                    rewardLabel.BackColor = Color.LightCyan;
+                    door1.Enabled = false;
+                    door2.Enabled = false;
+                    door3.Enabled = false;
+                }
+                else if(randomDoor == 1)
+                {
+                    door2.Tag = 0;
+                    door3.Tag = 2;
+                    door1.Text = medium_prizes[randomPrize2];
+                    rewardLabel.Text = "You won " + medium_prizes[randomPrize2] + "." + "You can change doors now or get the prize!";
+                    rewardLabel.BackColor = Color.LightCyan;
+                    door1.Enabled = false;
+                    door2.Enabled = false;
+                    door3.Enabled = false;
+                }
+                else
+                {
+                    door2.Tag = 1;
+                    door3.Tag = 0;
+                    door1.Text = amazing_prizes[randomPrize3];
+                    rewardLabel.Text = "You won " + amazing_prizes[randomPrize3] + "." + "You can change doors now or get the prize!";
+                    rewardLabel.BackColor = Color.LightCyan;
+                    door1.Enabled = false;
+                    door2.Enabled = false;
+                    door3.Enabled = false;
+                }
+            }
+        }
+
+        private void Door2_Click(object sender, EventArgs e)
+        {
+            choiceButton.Enabled = true;
+            door2WasClicked = true;
+            if (choice)
+            {
+                door2.Tag = randomDoor;
+                if (randomDoor == 0)
+                {
+                    door1.Tag = 2;
+                    door3.Tag = 1;
+                    door2.Text = trash_prizes[randomPrize1];
+                    rewardLabel.Text = "You won "+ trash_prizes[randomPrize1] + "." + "You can change doors now or get the prize!";
+                    rewardLabel.BackColor = Color.LightCyan;
+                    door2.Enabled = false;
+                    door1.Enabled = false;
+                    door3.Enabled = false;
+                }
+                else if (randomDoor == 1)
+                {
+                    door1.Tag = 0;
+                    door3.Tag = 2;
+                    door2.Text = medium_prizes[randomPrize2];
+                    rewardLabel.Text = "You won " + medium_prizes[randomPrize2] + "." + "You can change doors now or get the prize!";
+                    rewardLabel.BackColor = Color.LightCyan;
+                    door2.Enabled = false;
+                    door1.Enabled = false;
+                    door3.Enabled = false;
+                }
+                else
+                {
+                    door1.Tag = 1;
+                    door3.Tag = 0;
+                    door2.Text = amazing_prizes[randomPrize3];
+                    rewardLabel.Text = "You won " + amazing_prizes[randomPrize3] + "." + "You can change doors now or get the prize!";
+                    rewardLabel.BackColor = Color.LightCyan;
+                    door2.Enabled = false;
+                    door1.Enabled = false;
+                    door3.Enabled = false;
+                }
+            }
+        }
+
+        private void Door3_Click(object sender, EventArgs e)
+        {
+            choiceButton.Enabled = true;
+            door3WasClicked = true;
+            if (choice)
+            {
+                door3.Tag = randomDoor;
+                if (randomDoor == 0)
+                {
+                    door1.Tag = 2;
+                    door2.Tag = 1;
+                    door3.Text = trash_prizes[randomPrize1];
+                    rewardLabel.Text = "You won " + trash_prizes[randomPrize1] + "." + "You can change doors now or get the prize!";
+                    rewardLabel.BackColor = Color.LightCyan;
+                    door3.Enabled = false;
+                    door2.Enabled = false;
+                    door1.Enabled = false;
+                }
+                else if (randomDoor == 1)
+                {
+                    door1.Tag = 0;
+                    door2.Tag = 2;
+                    door3.Text = medium_prizes[randomPrize2];
+                    rewardLabel.Text = "You won " + medium_prizes[randomPrize2] + "." + "You can change doors now or get the prize!";
+                    rewardLabel.BackColor = Color.LightCyan;
+                    door3.Enabled = false;
+                    door2.Enabled = false;
+                    door1.Enabled = false;
+                }
+                else
+                {
+                    door1.Tag = 1;
+                    door2.Tag = 0;
+                    door3.Text = amazing_prizes[randomPrize3];
+                    rewardLabel.Text = "You won " + amazing_prizes[randomPrize3] + "." + "You can change doors now or get the prize!";
+                    rewardLabel.BackColor = Color.LightCyan;
+                    door3.Enabled = false;
+                    door2.Enabled = false;
+                    door1.Enabled = false;
+                }
+            }
+        }
+
+
+        private void ChoiceButton_Click(object sender, EventArgs e)
+        {
+            
+                Random r = new Random();
+                Random d = new Random();
+                randomDoor = d.Next(0, 3);
+                randomPrize1 = r.Next(0, trash_prizes.Length);
+                randomPrize2 = r.Next(0, medium_prizes.Length);
+                randomPrize3 = r.Next(0, amazing_prizes.Length);
+                choiceButton.Enabled = false;
+                rewardLabel.Text = String.Empty;
+                rewardLabel.BackColor = Color.Empty;
+                if (door1WasClicked)
+                {
+                    door2.Enabled = true;
+                    door3.Enabled = true;
+                    choiceButton.Enabled = false;
+                    if (door2WasClicked)
+                    {
+                        door2.Text = medium_prizes[randomPrize2];
+                        rewardLabel.Text = "Congratulation! You won " + amazing_prizes[randomPrize2] + ".";
+                        choiceButton.Enabled = false;
+                        door3.Text = amazing_prizes[randomPrize3];
+                        door2.Enabled = false;
+                        door3.Enabled = false;
+                    }
+                    if (door3WasClicked)
+                    {
+                        door3.Text = amazing_prizes[randomPrize3];
+                        rewardLabel.Text = "Congratulation! You won " + amazing_prizes[randomPrize3] + ".";
+                        choiceButton.Enabled = false;
+                        door2.Text = medium_prizes[randomPrize2];
+                        door2.Enabled = false;
+                        door3.Enabled = false;
+                    }
+                }
+                else if (door2WasClicked)
+                {
+                    door1.Enabled = true;
+                    door3.Enabled = true;
+                    choiceButton.Enabled = false;
+                    if (door1WasClicked)
+                    {
+                        door1.Text = medium_prizes[randomPrize2];
+                        rewardLabel.Text = "Congratulation! You won " + amazing_prizes[randomPrize2] + ".";
+                        choiceButton.Enabled = false;
+                        door3.Text = amazing_prizes[randomPrize3];
+                        door2.Enabled = false;
+                        door3.Enabled = false;
+                    }
+                    if (door3WasClicked)
+                    {
+                        door3.Text = trash_prizes[randomPrize1];
+                        rewardLabel.Text = "Congratulation! You won " + trash_prizes[randomPrize1] + ".";
+                        choiceButton.Enabled = false;
+                        door1.Text = medium_prizes[randomPrize2];
+                        door1.Enabled = false;
+                        door3.Enabled = false;
+                    }
+                }
+                else if (door3WasClicked)
+                {
+                    door1.Enabled = true;
+                    door2.Enabled = true;
+                    choiceButton.Enabled = false;
+                    if (door2WasClicked)
+                    {
+                        door2.Text = amazing_prizes[randomPrize3];
+                        rewardLabel.Text = "Congratulation! You won " + amazing_prizes[randomPrize3] + ".";
+                        choiceButton.Enabled = false;
+                        door1.Text = medium_prizes[randomPrize2];
+                        door2.Enabled = false;
+                        door1.Enabled = false;
+                    }
+                    if (door1WasClicked)
+                    {
+                        door1.Text = trash_prizes[randomPrize1];
+                        rewardLabel.Text = "Congratulation! You won " + trash_prizes[randomPrize1] + ".";
+                        choiceButton.Enabled = false;
+                        door2.Text = medium_prizes[randomPrize2];
+                        door2.Enabled = false;
+                        door1.Enabled = false;
+                    }
+                }
+            
+
+        }
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            Random d = new Random();
+            randomDoor = d.Next(0, 3);
+            randomPrize1 = r.Next(0, trash_prizes.Length);
+            randomPrize2 = r.Next(0, medium_prizes.Length);
+            randomPrize3 = r.Next(0, amazing_prizes.Length);
+            door1.Text = "Door 1";
+            door2.Text = "Door 2";
+            door3.Text = "Door 3";
+            rewardLabel.Text = String.Empty;
+            rewardLabel.BackColor = Color.Empty;
+            door1.Enabled = true;
+            door2.Enabled = true;
+            door3.Enabled = true;
+            door1WasClicked = false;
+            door2WasClicked = false;
+            door3WasClicked = false;
+        }
+
+    }
+}
+//That Tung Ton
